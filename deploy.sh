6afbfc187c368b00293c1f15fa0da920ce2d72dd
tomcohen <66440442+tomcoh5@users.noreply.github.com>
2020-09-01 23:59:12 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /var/jenkins_home/pipeline-for-pushing-to-ecr
-cluster='flask-up'
 ecs-cli compose up --create-log-groups --cluster-config $cluster  --ecs-profile $cluster-profile
-#ecs-cli compose scale 2 --cluster-config $name  --ecs-proile $name-profile
+sleep 40
+ecs-cli compose scale 2 --cluster-config $name  --ecs-proile $name-profile
