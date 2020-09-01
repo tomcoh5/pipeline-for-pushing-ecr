5e41f76f8a77f57d7f75e2d8fffacb204f0ef6de
tomcohen <66440442+tomcoh5@users.noreply.github.com>
2020-09-02 00:02:49 +0300

    Update ecs.sh

--- ecs.sh
+++ ecs.sh
@@ -2,4 +2,4 @@
 echo "please make sure you have ecs-cli-installed"
 ecs-cli configure --cluster $cluster --default-launch-type EC2 --config-name $cluster --region $region
 ecs-cli configure profile --access-key $access_key_id  --secret-key $secret_access_key  --profile-name $cluster-profile
-ecs-cli up --force  --keypair elasticsearch  --capability-iam --size 2 --instance-type t2.medium --cluster-config $cluster  --ecs-profile $cluster-profile
+ecs-cli up --force  --keypair $keypair  --capability-iam --size $numberofinstance --instance-type t2.medium --cluster-config $cluster  --ecs-profile $cluster-profile
